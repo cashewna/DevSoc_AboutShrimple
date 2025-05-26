415dc5cd75f8d9a6ec70313219574c29db2856ad
Krishna Khong <krishnakhong@gmail.com>
2025-05-27 00:07:44 +1000

    initialise repo

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Dango from "../../public/dango.svg";
+import style from "./Header.module.css";
+
+function Header() {
+    return (
+        <header className={style.header}>
+            <img src={Dango} alt="Dango" width="64rem"/>
+            <h1>Opposites Attract</h1>
+        </header>
+    )
+};
+
+export { Header };
